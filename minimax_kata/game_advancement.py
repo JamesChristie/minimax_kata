2fda18a929a6db5e79ce424108e8040658dba67c
James Christie <james.aaron.christie@gmail.com>
2015-07-31 17:16:25 +0000

    Eliminate references to get_current_player()

--- minimax_kata/game_advancement.py
+++ minimax_kata/game_advancement.py
@@ -5,16 +5,13 @@ class GameAdvancement:
   def __init__(self, game, move):
     self.game              = game
     self.move              = move
-    self.original_position = self.get_current_player().position
-
-  def get_current_player(self):
-    return self.move.player
+    self.original_position = self.move.player.position
 
   def opposing_player(self):
     return next(
       player for player
       in [self.game.player1, self.game.player2]
-      if player != self.get_current_player()
+      if player != self.move.player
     )
 
   def do(self):
@@ -28,12 +25,12 @@ class GameAdvancement:
   # Private
 
   def __move_player(self):
-    self.get_current_player().position  = self.move.new_position()
-    self.get_current_player().direction = self.move.direction
+    self.move.player.position  = self.move.new_position()
+    self.move.player.direction = self.move.direction
 
   def __claim_arena_space(self):
     self.game.claim_space_for(
-      self.get_current_player(), self.original_position
+      self.move.player, self.original_position
     )
 
   def __swap_current_player(self):
