a5f3544a82f7f941768391130cb428223f99f34d
James Christie <james.aaron.christie@gmail.com>
2015-09-11 14:47:39 +0000

    Add full kata suite in commented form

--- tests/test_minimax_solver.py
+++ tests/test_minimax_solver.py
@@ -72,3 +72,73 @@ class TestThatTieIsChosenInMirroredTwoByOne(unittest.TestCase):
       set(self.minimax_solver.get_best_moves()),
       set([DIRECTIONS.west])
     )
+
+# class TestThatWallIsAvoidedInTwoByThree(unittest.TestCase):
+#   def setUp(self):
+#     self.game_string  = """\
+#       ████████
+#       ██↓ ☐ ██
+#       ██☐ ☐ ██
+#       ██☐ ▲ ██
+#       ████████"""
+# 
+#     self.game = GameInterpreter(self.game_string.replace("      ", "")).do()
+#     self.minimax_solver = MinimaxSolver(self.game)
+# 
+#   def test_best_moves(self):
+#     self.assertEqual(
+#       set(self.minimax_solver.get_best_moves()),
+#       set([DIRECTIONS.south, DIRECTIONS.east])
+#     )
+# 
+# class TestThatTrailsIsAvoidedInTwoByTwo(unittest.TestCase):
+#   def setUp(self):
+#     self.game_string  = """\
+#       ████████
+#       ██↓ ② ██
+#       ██☐ ▼ ██
+#       ████████"""
+# 
+#     self.game = GameInterpreter(self.game_string.replace("      ", "")).do()
+#     self.minimax_solver = MinimaxSolver(self.game)
+# 
+#   def test_best_moves(self):
+#     self.assertEqual(
+#       set(self.minimax_solver.get_best_moves()),
+#       set([DIRECTIONS.south])
+#     )
+# 
+# class TestThatPlayerIsAvoided(unittest.TestCase):
+#   def setUp(self):
+#     self.game_string  = """\
+#       ████████
+#       ██① ② ██
+#       ██↓ ▼ ██
+#       ██☐ ████
+#       ████████"""
+# 
+#     self.game = GameInterpreter(self.game_string.replace("      ", "")).do()
+#     self.minimax_solver = MinimaxSolver(self.game)
+# 
+#   def test_best_moves(self):
+#     self.assertEqual(
+#       set(self.minimax_solver.get_best_moves()),
+#       set([DIRECTIONS.south])
+#     )
+# 
+# class TestAvoidingDeadEnd(unittest.TestCase):
+#   def setUp(self):
+#     self.game_string  = """\
+#       ████████████
+#       ██→ ☐ ☐ ▲ ██
+#       ██☐ ████② ██
+#       ████████████"""
+# 
+#     self.game = GameInterpreter(self.game_string.replace("      ", "")).do()
+#     self.minimax_solver = MinimaxSolver(self.game)
+# 
+#   def test_best_moves(self):
+#     self.assertEqual(
+#       set(self.minimax_solver.get_best_moves()),
+#       set([DIRECTIONS.east])
+#     )
