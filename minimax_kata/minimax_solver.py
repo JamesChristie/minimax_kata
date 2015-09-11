b0c75e4a1456afa8975a6ea25aa0d3efa0e7967d
James Christie <james.aaron.christie@gmail.com>
2015-09-11 14:20:16 +0000

    Step 4

--- minimax_kata/minimax_solver.py
+++ minimax_kata/minimax_solver.py
@@ -2,6 +2,10 @@ from minimax_kata.arena import DIRECTIONS
 
 from minimax_kata.potential_game_presenter import get_potential_games
 
+ALIVE_SCORE = 10
+CRASH_SCORE = 0
+DEATH_SCORE = -10
+
 class MinimaxSolver:
   def __init__(self, game):
     self.game = game
@@ -11,5 +15,19 @@ class MinimaxSolver:
     return [
       presenter.direction for presenter
       in self.potential_games
-      if not presenter.is_wall() and not presenter.is_trail()
+      if self.__get_score(presenter) == self.get_max_score()
     ]
+
+  def get_max_score(self):
+    return max(
+      self.__get_score(presenter) for presenter
+      in self.potential_games
+    )
+
+  def __get_score(self, presenter):
+    if presenter.is_alive():
+      return ALIVE_SCORE
+    elif presenter.is_crash():
+      return CRASH_SCORE
+    else:
+      return DEATH_SCORE
--- tests/test_minimax_solver.py
+++ tests/test_minimax_solver.py
@@ -108,24 +108,24 @@ class TestThatTrailsIsAvoidedInTwoByTwo(unittest.TestCase):
       set([DIRECTIONS.south])
     )
 
-# class TestThatPlayerIsAvoided(unittest.TestCase):
-#   def setUp(self):
-#     self.game_string  = """\
-#       ████████
-#       ██① ② ██
-#       ██↓ ▼ ██
-#       ██☐ ████
-#       ████████"""
-# 
-#     self.game = GameInterpreter(self.game_string.replace("      ", "")).do()
-#     self.minimax_solver = MinimaxSolver(self.game)
-# 
-#   def test_best_moves(self):
-#     self.assertEqual(
-#       set(self.minimax_solver.get_best_moves()),
-#       set([DIRECTIONS.south])
-#     )
-# 
+class TestThatPlayerIsAvoided(unittest.TestCase):
+  def setUp(self):
+    self.game_string  = """\
+      ████████
+      ██① ② ██
+      ██↓ ▼ ██
+      ██☐ ████
+      ████████"""
+
+    self.game = GameInterpreter(self.game_string.replace("      ", "")).do()
+    self.minimax_solver = MinimaxSolver(self.game)
+
+  def test_best_moves(self):
+    self.assertEqual(
+      set(self.minimax_solver.get_best_moves()),
+      set([DIRECTIONS.south])
+    )
+
 # class TestAvoidingDeadEnd(unittest.TestCase):
 #   def setUp(self):
 #     self.game_string  = """\
