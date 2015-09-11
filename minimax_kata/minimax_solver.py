40f7d5502384f01b75e65cb55f5fc3d2b1e73ca1
James Christie <james.aaron.christie@gmail.com>
2015-09-11 14:10:15 +0000

    Step 1

--- minimax_kata/minimax_solver.py
+++ minimax_kata/minimax_solver.py
@@ -1,6 +1,17 @@
+from minimax_kata.arena import DIRECTIONS
+
 class MinimaxSolver:
   def __init__(self, game):
     self.game = game
 
   def get_best_moves(self):
-    pass
+    available = self.game.get_available_directions()
+
+    if DIRECTIONS.north not in available:
+      return [DIRECTIONS.south]
+    elif DIRECTIONS.east not in available:
+      return [DIRECTIONS.west]
+    elif DIRECTIONS.south not in available:
+      return [DIRECTIONS.north]
+    else:
+      return [DIRECTIONS.east]
